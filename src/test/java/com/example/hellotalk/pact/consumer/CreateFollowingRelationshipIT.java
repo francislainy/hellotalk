af1d21abf7a1168a0b497df95d27025c8fdb8304
Francislainy <francislainy.campos@gmail.com>
2023-01-08 15:28:36 +0000

    Rename create following relationship file

--- src/test/java/com/example/hellotalk/pact/consumer/CreateFollowingRelationshipIT.java
+++ src/test/java/com/example/hellotalk/pact/consumer/CreateFollowingRelationshipIT.java
@@ -23,7 +23,7 @@ import static java.util.UUID.fromString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(PactConsumerTestExt.class)
-class FollowUserIT {
+class CreateFollowingRelationshipIT {
 
     Map<String, String> headers = new HashMap<>();
 

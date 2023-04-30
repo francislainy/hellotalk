d90e94ddd710994ef853f84bf7d512a6816d6d76
Francislainy <francislainy.campos@gmail.com>
2023-04-30 18:28:02 +0100

    Have ApplicationTests extend BasePostgresConfig to debug failed test on github actions

--- src/test/java/com/example/hellotalk/HelloTalkApplicationTests.java
+++ src/test/java/com/example/hellotalk/HelloTalkApplicationTests.java
@@ -1,12 +1,14 @@
 package com.example.hellotalk;
 
+import com.example.hellotalk.config.BasePostgresConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HelloTalkApplicationTests {
+class HelloTalkApplicationTests extends BasePostgresConfig {
 
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+    }
 
 }

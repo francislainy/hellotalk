157fa19f9ef3dc5cf13ac45b06a336a3b70cfbc3
Francislainy <francislainy.campos@gmail.com>
2023-08-10 12:39:46 +0100

    Add user cant follow themself functionality

--- src/test/java/com/example/hellotalk/controller/follow/FollowingRequestControllerTest.java
+++ src/test/java/com/example/hellotalk/controller/follow/FollowingRequestControllerTest.java
@@ -1,8 +1,8 @@
 package com.example.hellotalk.controller.follow;
 
 import com.example.hellotalk.controller.BaseTestConfig;
-import com.example.hellotalk.entity.user.UserEntity;
 import com.example.hellotalk.exception.FollowingRelationshipDeletedException;
+import com.example.hellotalk.exception.FollowingRelationshipNotCreatedException;
 import com.example.hellotalk.model.FollowingRequest;
 import com.example.hellotalk.repository.UserRepository;
 import com.example.hellotalk.service.FollowingRequestService;
@@ -18,10 +18,11 @@ import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+import static com.example.hellotalk.exception.AppExceptionHandler.FOLLOWING_RELATIONSHIP_ALREADY_EXISTS_EXCEPTION;
+import static com.example.hellotalk.exception.AppExceptionHandler.USER_TO_AND_FROM_CANT_BE_THE_SAME;
 import static com.example.hellotalk.util.Utils.jsonStringFromObject;
 import static java.util.UUID.randomUUID;
 import static org.mockito.ArgumentMatchers.any;
@@ -29,6 +30,9 @@ import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 
 @WebMvcTest(controllers = FollowingRequestController.class)
 @ExtendWith(MockitoExtension.class)
@@ -110,18 +114,13 @@ class FollowingRequestControllerTest extends BaseTestConfig {
     }
 
     @Test
-    void testCreateFollowingRequest_WhenRelationshipDoesNotYetExist_RelationshipCreatedAndSuccessReturned() throws Exception {
+    void testCreateFollowingRequest_WhenFollowingRelationshipDoesNotYetExist_ReturnsSuccess() throws Exception {
         UUID userToId = randomUUID();
         UUID userFromId = randomUUID();
 
         FollowingRequest followingRequest = FollowingRequest.builder().userToId(userToId).userFromId(userFromId).build();
         FollowingRequest followingResponse = FollowingRequest.builder().id(userToId).userToId(userToId).userFromId(userFromId).build();
 
-        UserEntity userFromEntity = UserEntity.builder().id(userFromId).build();
-        UserEntity userToEntity = UserEntity.builder().id(userToId).build();
-
-        when(userRepository.findById(userFromEntity.getId())).thenReturn(Optional.of(userFromEntity));
-        when(userRepository.findById(userToEntity.getId())).thenReturn(Optional.of(userToEntity));
         when(followingRequestService.createFollowingRequest(any())).thenReturn(followingResponse);
 
         String jsonRequest = jsonStringFromObject(followingRequest);
@@ -135,24 +134,38 @@ class FollowingRequestControllerTest extends BaseTestConfig {
     }
 
     @Test
-    void testCreateFollowingRequest_WhenRelationshipAlreadyExists_DeletesRelationshipAndReturnsSuccess() throws Exception {
+    void testCreateFollowingRequest_WhenFollowingRelationDeletedExceptionIsThrown_ReturnsSuccess() throws Exception {
         UUID userToId = randomUUID();
         UUID userFromId = randomUUID();
-
         FollowingRequest followingRequest = FollowingRequest.builder().userToId(userToId).userFromId(userFromId).build();
 
-        UserEntity userFromEntity = UserEntity.builder().id(userFromId).build();
-        UserEntity userToEntity = UserEntity.builder().id(userToId).build();
-
-        when(userRepository.findById(userFromEntity.getId())).thenReturn(Optional.of(userFromEntity));
-        when(userRepository.findById(userToEntity.getId())).thenReturn(Optional.of(userToEntity));
         when(followingRequestService.createFollowingRequest(any())).thenThrow(FollowingRelationshipDeletedException.class);
 
         String jsonRequest = jsonStringFromObject(followingRequest);
         mockMvc.perform(RestDocumentationRequestBuilders.post("/api/v1/ht/follow/").content(jsonRequest).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isPartialContent())
-                .andDo(document("create-following-request-for-user",
-                        resource("Create a following request for a user")))
+                .andDo(document("create-following-request-for-user-to-unfollow-and-delete-existing-relationship-",
+                        resource("Create a following request for a user to unfollow another user")))
                 .andReturn();
     }
+
+    @Test
+    void testCreateFollowingRequest_FollowingRelationshipNotCreatedException_Returns400BadRequestError() throws Exception {
+
+        UUID userToId = randomUUID();
+        UUID userFromId = randomUUID();
+
+        FollowingRequest followingRequest = FollowingRequest.builder().userToId(userToId).userFromId(userFromId).build();
+
+        when(followingRequestService.createFollowingRequest(any())).thenThrow(new FollowingRelationshipNotCreatedException(USER_TO_AND_FROM_CANT_BE_THE_SAME));
+
+        String jsonRequest = jsonStringFromObject(followingRequest);
+        mockMvc.perform(RestDocumentationRequestBuilders.post("/api/v1/ht/follow/").content(jsonRequest).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", is(USER_TO_AND_FROM_CANT_BE_THE_SAME)))
+                .andDo(document("create-following-request-for-user-fails-when-user-is-the-same",
+                        resource("Create a following request for a user to follow themself - It should fail")))
+                .andReturn();
+    }
+
 }

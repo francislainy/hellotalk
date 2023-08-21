85d61cc12a73153379a1d90651f3f646b8aa0281
Francislainy <francislainy.campos@gmail.com>
2023-08-21 03:17:01 +0100

    Small removal of spaces

--- src/test/java/com/example/hellotalk/repository/moment/MomentRepositoryTest.java
+++ src/test/java/com/example/hellotalk/repository/moment/MomentRepositoryTest.java
@@ -28,22 +28,20 @@ class MomentRepositoryTest {
         UserEntity userEntity1 = UserEntity.builder().name("user1").build();
         userEntity1 = userRepository.save(userEntity1);
 
-        MomentEntity moment1 = new MomentEntity();
-        moment1.setUserEntity(userEntity1);
-        momentRepository.save(moment1);
+        MomentEntity momentEntity1 = new MomentEntity();
+        momentEntity1.setUserEntity(userEntity1);
+        momentRepository.save(momentEntity1);
 
         UserEntity userEntity2 = UserEntity.builder().name("user2").build();
         userEntity2 = userRepository.save(userEntity2);
 
-        MomentEntity moment2 = new MomentEntity();
-        moment2.setUserEntity(userEntity2);
-        momentRepository.save(moment2);
+        MomentEntity momentEntity2 = new MomentEntity();
+        momentEntity2.setUserEntity(userEntity2);
+        momentRepository.save(momentEntity2);
 
-        // Act
         List<MomentEntity> moments = momentRepository.findAllByUserEntity_IdContains(userEntity1.getId());
 
-        // Assert
         assertEquals(1, moments.size());
-        assertEquals(moments.get(0).getId(), moment1.getId());
+        assertEquals(moments.get(0).getId(), momentEntity1.getId());
     }
 }

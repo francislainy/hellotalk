361b80baba7abc0000d264caa2fe5c3641f69f64
Francislainy <francislainy.campos@gmail.com>
2023-08-23 00:46:31 +0100

    Small clean up on repository classes

--- src/main/java/com/example/hellotalk/repository/LikeRepository.java
+++ src/main/java/com/example/hellotalk/repository/LikeRepository.java
@@ -4,10 +4,12 @@ import com.example.hellotalk.entity.user.LikeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface LikeRepository extends JpaRepository<LikeEntity, UUID> {
 
     List<LikeEntity> findAllByMomentEntityIdContaining(UUID momentId);

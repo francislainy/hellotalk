5457fca36c2bffb60f9f58de3b8de1c075cef628
Francislainy <francislainy.campos@gmail.com>
2023-09-29 03:32:05 +0100

    wip update to spring boot 3.1.4 for latest testcontainers

--- src/test/java/com/example/hellotalk/config/BasePostgresConfig.java
+++ src/test/java/com/example/hellotalk/config/BasePostgresConfig.java
@@ -1,17 +1,15 @@
 package com.example.hellotalk.config;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 
 @ActiveProfiles("test")
 public class BasePostgresConfig {
 
     @Container
-    public static PostgresSqlContainer postgres = PostgresSqlContainer.getInstance();
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:13.5");
 
-    @BeforeAll
-    public static void init() {
-        postgres.start();
-    }
 }
